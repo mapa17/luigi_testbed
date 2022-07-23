02446d1105c9ced9c123f2a5ad604e129a5e54ea
Pasieka Manuel <manuel.pasieka@protonmail.ch>
2022-07-23 15:28:42 +0200

    Getting rid of wrong and unused parameters.

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -5,8 +5,6 @@ def test_pipeline():
     tasks = [RunPipeline()]
 
     # Create the pipeline. This will execute the tasks
-    luigi.build(tasks, scheduler_host='localhost',
-                       scheduler_port='8888',
-                       local_scheduler=True)
+    luigi.build(tasks, local_scheduler=True)
 
 test_pipeline()
